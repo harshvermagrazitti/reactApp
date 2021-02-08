d77bc4928ac200a36e77399f72eaee990ebb82b2
harshvermagrazitti <harsh.verma@grazitti.com>
2021-02-08 16:58:11 +0530

    json

--- App.js
+++ App.js
@@ -1,40 +1,62 @@
-import React, { Component } from 'react'
-import { View, StyleSheet, Animated, TouchableOpacity } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-class Animations extends Component {
-   componentWillMount = () => {
-      this.animatedWidth = new Animated.Value(50)
-      this.animatedHeight = new Animated.Value(100)
-   }
-   animatedBox = () => {
-      Animated.timing(this.animatedWidth, {
-         toValue: 200,
-         duration: 1000
-      }).start()
-      Animated.timing(this.animatedHeight, {
-         toValue: 500,
-         duration: 500
-      }).start()
-   }
-   render() {
-      const animatedStyle = { width: this.animatedWidth, height: this.animatedHeight }
-      return (
-         <TouchableOpacity style = {styles.container} onPress = {this.animatedBox}>
-            <Animated.View style = {[styles.box, animatedStyle]}/>
-         </TouchableOpacity>
-      )
-   }
+export default function App() {
+
+  const [newTask, setnewTask] = useState('');
+
+  const [appTasks, appTask] = useState([]);
+
+  const taskInputHandler = (enteredText) => {
+    setnewTask(enteredText);
+  };
+
+  const addTaskHandler = () =>{
+    appTask(currentTask => [...currentTask, newTask]);
+    console.log(newTask);
+  };
+
+  return (
+    <View style= {styles.container}>
+
+      <View style = {styles.inputContainer}>
+
+        <TextInput
+          placeholder = "Task List"
+          style = {styles.input}
+          onChangeText = {taskInputHandler}
+          value = {newTask}
+        />
+
+        <Button title = "+"
+          onPress = {addTaskHandler}
+        />
+      </View>
+      <View>
+        {appTasks.map((task) => <Text>{task}</Text>)}
+      </View>
+    </View>
+  );
 }
-export default Animations
 
 const styles = StyleSheet.create({
-   container: {
-      justifyContent: 'center',
-      alignItems: 'center'
-   },
-   box: {
-      backgroundColor: 'blue',
-      width: 50,
-      height: 100
-   }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input :{
+    borderColor:"black",
+    borderWidth:1 ,
+    padding :20,
+
+  },
+  inputContainer :{
+    flexDirection :'row',
+    justifyContent :'space-between',
+    alignContent:'center',
+    bottom:20
+  },
+});
